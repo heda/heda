81709a30765be3d0698318bb46313ad064c12b57
Daniel <heda0031@googlemail.com>
2011-06-01 21:48:35 +0200

    Main Method implemented

--- src/de/heda/HelloWorld.java
+++ src/de/heda/HelloWorld.java
@@ -1,9 +1,15 @@
 package de.heda;
 
 public class HelloWorld {
-	
-	public static void main(String{} args){
-		System.out,println("Hello World");
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello World");
+
 	}
 
 }
